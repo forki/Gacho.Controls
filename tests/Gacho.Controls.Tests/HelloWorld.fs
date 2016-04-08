5109f03b59d1eae1b691d8aa7cba7e24d2925dca
Michael Gómez Campos <michakun@gmail.com>
2016-04-08 18:51:56 +0200

    Added test project with FsCheck + NUnit

---
+++ tests/Gacho.Controls.Tests/HelloWorld.fs
@@ -0,0 +1,7 @@
+﻿module Gacho.Controls.Test
+
+open FsCheck.NUnit
+
+[<Property>]
+let ``Reverse of reverse of a list is the original list ``(xs:list<int>) =
+  List.rev(List.rev xs) = xs
---
+++ tests/Gacho.Controls.Tests/Scripts/load-references-debug.fsx
@@ -0,0 +1,13 @@
+// Warning: generated file; your changes could be lost when a new file is generated.
+#I __SOURCE_DIRECTORY__
+#r "../../../packages/FsCheck/lib/net45/FsCheck.dll"
+#r "../../../packages/FsCheck.Nunit/lib/net45/FsCheck.NUnit.Addin.dll"
+#r "../../../packages/FsCheck.Nunit/lib/net45/FsCheck.NUnit.dll"
+#r "../../../packages/NUnitTestAdapter/lib/nunit.core.dll"
+#r "../../../packages/NUnitTestAdapter/lib/nunit.core.interfaces.dll"
+#r "../../../packages/NUnit/lib/nunit.framework.dll"
+#r "../../../packages/NUnitTestAdapter/lib/nunit.util.dll"
+#r "../../../packages/NUnitTestAdapter/lib/NUnit.VisualStudio.TestAdapter.dll"
+#r "System.Core.dll"
+#r "System.dll"
+#r "System.Numerics.dll"
